9aa8e43db9dc68d8e483022d6ff6b6c33dbbaf2d
Alvin Kariuki <alvinkariuki230@gmail.com>
2022-02-03 23:49:32 +0300

    Weird number

---
+++ python Code challenges/weird_number.py
@@ -0,0 +1,21 @@
+def isOdd(n):
+    if n % 2 == 0:
+        return False
+    else:
+        return True
+def rangeCheck(n, r1, r2):
+    R = range(r1, r2)
+    return n in R
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if isOdd(n):
+        print("Weird")
+    elif not isOdd(n) and rangeCheck(n, 2, 5):
+        print("Not Weird")
+    elif not isOdd(n) and rangeCheck(n, 6, 20):
+        print("Weird")
+    elif not isOdd(n) and n > 20:
+        print("Not weird")
